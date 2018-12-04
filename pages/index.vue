92d5d9ff69b0b686520a13c9139ed186d5b89d19
rehmpke <rehmpke@gmail.com>
2018-12-04 15:30:10 -0600

    Testing writing this code

---
+++ pages/index.vue
@@ -0,0 +1,133 @@
+<template>
+  <section class="container-fluid">
+    <div>
+      <b-navbar
+        toggleable="md"
+        type="dark"
+        variant="info"
+      >
+
+        <b-navbar-toggle
+          target="nav_collapse"
+        />
+
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-collapse
+          id="nav_collapse"
+          is-nav
+        >
+
+          <b-navbar-nav>
+            <b-nav-item
+              href="#"
+            >Link</b-nav-item>
+            <b-nav-item
+              href="#"
+              disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav
+            class="ml-auto">
+
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                type="text"
+                placeholder="Search"
+              />
+              <b-button
+                size="sm"
+                class="my-2 my-sm-0"
+                type="submit"
+              >Search</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown
+              text="Lang"
+              right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using button-content slot -->
+              <template slot="button-content">
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Signout</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+
+        </b-collapse>
+      </b-navbar>
+
+      <!-- navbar-1.vue -->
+      <logo/>
+      <h1 class="title">
+        Front-end Dev
+      </h1>
+      <h2 class="subtitle">
+        My doozie Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
